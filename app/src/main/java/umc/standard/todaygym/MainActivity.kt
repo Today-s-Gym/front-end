9afa1a53e3bccee827d297ec153768ad1744078a
kimwest00 <kimwest00@hufs.ac.kr>
2023-01-13 16:38:00 +0900

    feat - nav_host 설정

--- app/src/main/java/umc/standard/todaygym/MainActivity.kt
+++ app/src/main/java/umc/standard/todaygym/MainActivity.kt
@@ -3,6 +3,8 @@ package umc.standard.todaygym
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import umc.standard.todaygym.databinding.ActivityMainBinding
 import umc.standard.todaygym.util.CameraActivity
 
@@ -10,14 +12,16 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initBinding()
+        initNavigation()
+    }
+    private fun initBinding() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //카메라 테스트용 btn listener
-//        binding.btnCamera.setOnClickListener {
-//            val intent = Intent(this, CameraActivity::class.java)
-//            startActivity(intent)
-//        }
+    }
 
+    private fun initNavigation() {
+        NavigationUI.setupWithNavController(binding.navBar, findNavController(R.id.nav_host))
 
     }
 }
