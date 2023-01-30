102ac087aaa80aed2cce16ff604fafdf2a52813a
Noh sein <si2238@naver.com>
2023-01-30 23:58:55 +0900

    feat: community api

--- app/src/main/java/umc/standard/todaygym/presentation/community/BoardData.kt
+++ app/src/main/java/umc/standard/todaygym/presentation/community/BoardData.kt
@@ -3,8 +3,27 @@ package umc.standard.todaygym.presentation.community
 import android.widget.ImageView
 
 data class BoardData(
-    val nickname: String,
-    val title: String,
-    val desc: String,
+    val isSuccess: Boolean,
+    val code: Int,
+    val message: String,
+    val result: Result
+) {
+    data class Result(
+        val categoryName: String,
+        val postId: Int,
+        val title: String,
+        val content: String,
+        val postPhotoList: String,
+        val createdAt: String,
+        val likeCounts: Int,
+        val liked: Boolean,
+        val commentCounts: Int,
+        val writerAvatarImgUrl:String,
+        val writerNickName: String,
+        val recordId: Int,
+        val recordPhotoImgUrl: String,
+        val recordCreateAd: String,
+        val recordContent: String
+    )
+}
 
-)
