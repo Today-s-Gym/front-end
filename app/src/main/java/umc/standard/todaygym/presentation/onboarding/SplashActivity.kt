6facd3890fc596b1007bfc24d6ccd2196fdd4cf9
kimwest00 <kimwest00@hufs.ac.kr>
2023-02-07 09:28:32 +0900

    feat - 권할설정

--- app/src/main/java/umc/standard/todaygym/presentation/onboarding/SplashActivity.kt
+++ app/src/main/java/umc/standard/todaygym/presentation/onboarding/SplashActivity.kt
@@ -39,19 +39,18 @@ import umc.standard.todaygym.util.SharePreferences.Companion.prefs
 class SplashActivity:AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
     //권한 가져오기
-    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+//    val CAMERA_PERMISSION = arrayOf(Manifest.permission.CAMERA)
     val STORAGE_PERMISSION = arrayOf(
         Manifest.permission.READ_EXTERNAL_STORAGE,
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
-//        Manifest.permission.CAMERA
+        Manifest.permission.CAMERA
     )
     //권한 플래그값 정의
-    val PERM_CAMERA = 98
+//    val PERM_CAMERA = 98
     val PERM_STORAGE = 99
 
     //카메라와 갤러리를 호출하는 플래그
     val FLAG_REQ_CAMERA = 101
-
     // 카카오계정으로 로그인 공통 callback 구성
     // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
     val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
@@ -119,78 +118,7 @@ fun kakaoApi(token:OAuthToken){
 
         })
     }
-    fun checkPermission(permissions: Array<out String>, flag: Int): Boolean {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            for (permission in permissions) {
-                //만약 권한이 승인되어 있지 않다면 권한승인 요청을 사용에 화면에 호출합니다.
-                if (ContextCompat.checkSelfPermission(
-                        this@SplashActivity,
-                        permission
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    ActivityCompat.requestPermissions(this@SplashActivity, permissions, flag)
-                    return false
-                }
-            }
-        }
-        return true
-    }
-    fun permissionDialog(){
-        val dialog = Dialog(this@SplashActivity)
-        val dialogView = DialogPermissionBinding.inflate(LayoutInflater.from(this@SplashActivity))
-        dialog.setContentView(dialogView.root)
-        val params : WindowManager.LayoutParams? = dialog.window?.attributes;
-        params?.width = WindowManager.LayoutParams.MATCH_PARENT
-        params?.height = WindowManager.LayoutParams.WRAP_CONTENT
-        if (params != null) {
-            dialog.window?.setLayout(params.width,params.height)
-        }
 
-        dialog.show()
-        dialogView.btnPermissionNext.setOnClickListener{
-            Log.d("fddfffd","dsfsfs")
-            checkPermission(STORAGE_PERMISSION, PERM_STORAGE)
-//            checkPermission(CAMERA_PERMISSION,PERMm_CAMERA)
-        }
-    }
-
-    //checkPermission() 에서 ActivityCompat.requestPermissions 을 호출한 다음 사용자가 권한 허용여부를 선택하면 해당 메소드로 값이 전달 됩니다.
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            PERM_STORAGE -> {
-                for (grant in grantResults) {
-                    if (grant != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "저장소 권한을 승인하지않으면 앱을 실행할수없습니다", Toast.LENGTH_SHORT)
-                            .show()
-                        finish()
-                        return
-                    }
-                }
-                val Intent = Intent(this@SplashActivity, SignUpNicknameActivity::class.java)
-                startActivity(Intent)
-                finish()
-
-
-            }
-            PERM_CAMERA -> {
-                for (grant in grantResults) {
-                    if (grant != PackageManager.PERMISSION_GRANTED) {
-                        Toast.makeText(this, "카메라 권한을 승인해야지만 카메라를 사용할 수 있습니다.", Toast.LENGTH_SHORT)
-                            .show()
-                        return
-                    }
-                }
-                val Intent = Intent(this@SplashActivity, SignUpNicknameActivity::class.java)
-                startActivity(Intent)
-                finish()
-            }
-        }
-    }
     fun showBottomSheet(context: Context){
 
         val dialog = BottomSheetDialog(context)
@@ -236,7 +164,6 @@ fun kakaoApi(token:OAuthToken){
             }
 
 
-
         }
         dialogView.btnPolicyPersonal.setOnClickListener{
             if(policyPersonal){
@@ -263,14 +190,85 @@ fun kakaoApi(token:OAuthToken){
 //                Log.d("bottomsheet","true")
                 dialog.dismiss()
                 permissionDialog()
+
             }else{
 //                Log.d("bottomsheet","false")
-                Toast.makeText(context,"약관에 동의해야 서비스를 이용할수있습니다",Toast.LENGTH_LONG).show()
+                Toast.makeText(this@SplashActivity,"약관에 동의해야 서비스를 이용할수있습니다",Toast.LENGTH_LONG).show()
             }
         }
         dialogView.btnCloseTap.setOnClickListener{
-            Toast.makeText(context,"약관에 동의해야 서비스를 이용할수있습니다",Toast.LENGTH_LONG).show()
+            Toast.makeText(this@SplashActivity,"약관에 동의해야 서비스를 이용할수있습니다",Toast.LENGTH_LONG).show()
 //        dialog.dismiss()
+        }
+    }
+
+
+    fun permissionDialog(){
+        val dialog = Dialog(this@SplashActivity)
+        val dialogView = DialogPermissionBinding.inflate(LayoutInflater.from(this@SplashActivity))
+        dialog.setContentView(dialogView.root)
+        val params : WindowManager.LayoutParams? = dialog.window?.attributes;
+        params?.width = WindowManager.LayoutParams.MATCH_PARENT
+        params?.height = WindowManager.LayoutParams.WRAP_CONTENT
+        if (params != null) {
+            dialog.window?.setLayout(params.width,params.height)
+        }
+        dialog.show()
+        dialogView.btnPermissionNext.setOnClickListener{
+            Log.d("fddfffd","dsfsfs")
+            checkPermission(STORAGE_PERMISSION, PERM_STORAGE)
+//            checkPermission(CAMERA_PERMISSION,PERMm_CAMERA)
+        }
+    }
+
+    fun checkPermission(permissions: Array<out String>, flag: Int): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Log.d("permission","1")
+            for (permission in permissions) {
+                //만약 권한이 승인되어 있지 않다면 권한승인 요청을 사용에 화면에 호출합니다.
+                if (ContextCompat.checkSelfPermission(
+                        this@SplashActivity,
+                        permission
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Log.d("permission","isGrandted?")
+                    ActivityCompat.requestPermissions(this, permissions, flag)
+                    Log.d("permission","isGrandted?")
+                    return false
+                }
+            }
+        }
+        Log.d("permission","1.5")
+        val Intent = Intent(this@SplashActivity, SignUpNicknameActivity::class.java)
+        startActivity(Intent)
+        finish()
+        return true
+    }
+    //checkPermission() 에서 ActivityCompat.requestPermissions 을 호출한 다음 사용자가 권한 허용여부를 선택하면 해당 메소드로 값이 전달 됩니다.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        val Intent = Intent(this@SplashActivity, SignUpNicknameActivity::class.java)
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERM_STORAGE -> {
+                Log.d("permission","2")
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "저장소 권한을 승인하지않으면 앱을 실행할수없습니다", Toast.LENGTH_SHORT)
+                            .show()
+                        finish()
+                        return
+                    }
+                }
+                startActivity(Intent)
+                finish()
+
+
+            }
+
         }
     }
 }
