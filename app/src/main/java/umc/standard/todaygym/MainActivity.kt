2b06f6d4b9ebed219aa3e5db6882eefce5eb1623
준써크 <junsuk8960@naver.com>
2023-02-14 20:44:23 +0900

    feat - 기록 수정/추가 api

--- app/src/main/java/umc/standard/todaygym/MainActivity.kt
+++ app/src/main/java/umc/standard/todaygym/MainActivity.kt
@@ -1,5 +1,7 @@
 package umc.standard.todaygym
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
